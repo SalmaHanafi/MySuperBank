4ee64d12198c9e0a4b9f23fec66cdb4e159c8e62
Salma Hanafi <shanafi2017@gmail.com>
2021-02-15 11:32:25 +0200

    Finishing up the basics

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Salma", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(200, DateTime.Now, "Chair");
+            account.MakeWithdrawal(300, DateTime.Now, "Glasses");
+            Console.WriteLine(account.GetAccountHistory());
         }
     }
 }
