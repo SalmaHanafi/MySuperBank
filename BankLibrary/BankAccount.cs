71737d3bf6324e06128f7bcd5e37f8aaa2419746
Salma Hanafi <shanafi2017@gmail.com>
2021-02-16 11:49:39 +0200

    adding BankLibrary project to sln

---
+++ BankLibrary/BankAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public class BankAccount
+    {
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }  }
+        private static int accountNumberSeed = 1234567890;
+       
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var history = new StringBuilder();
+            history.AppendLine("Date\t\tAmount\t\t\tNote");
+            foreach (var item in allTransactions)
+            {
+                history.AppendLine($"{item.Date.ToShortDateString()}\t{item.AmountForHumans}\t\t{item.Notes}");
+            }
+            return history.ToString();
+        }
+
+    
+    }
+}
