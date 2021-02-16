a6c0b121430b816209cc41f0703df2fb1c610a3f
Salma Hanafi <shanafi2017@gmail.com>
2021-02-16 10:31:12 +0200

    adding a library

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using BankLibrary;
 
 namespace MySuperBank
+
 {
     class Program
     {
@@ -10,8 +12,15 @@ namespace MySuperBank
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
 
             account.MakeWithdrawal(200, DateTime.Now, "Chair");
+            account.MakeWithdrawal(7, DateTime.Now, "Coffee");
+            account.MakeWithdrawal(5, DateTime.Now, "Snack");
+            account.MakeWithdrawal(1, DateTime.Now, "snack2");
             account.MakeWithdrawal(300, DateTime.Now, "Glasses");
+            account.MakeWithdrawal(5, DateTime.Now, "Coke");
+
+
             Console.WriteLine(account.GetAccountHistory());
+
         }
     }
 }
